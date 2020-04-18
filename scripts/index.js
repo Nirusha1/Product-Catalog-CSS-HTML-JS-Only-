c638bc70c2668e31d96907087bb41e54a3fae205
Nirusha1 <bhintunasayami123@gmail.com>
2020-04-18 09:21:42 +0545

    added plugins

---
+++ scripts/index.js
@@ -0,0 +1,83 @@
+
+function watch(){
+            document.getElementById("cell").innerHTML="Watch";
+            document.getElementById("cell1").src="https://images-na.ssl-images-amazon.com/images/I/71gdBQP%2BqGL._UY445_.jpg";
+            document.getElementById("cell2").src="https://images-na.ssl-images-amazon.com/images/I/71efcy6N1dL._UY445_.jpg";
+             document.getElementById("cell3").src="https://images-na.ssl-images-amazon.com/images/I/719D%2BJT0XRL._UY550_.jpg";
+            document.getElementById("cell1_price").innerHTML=" &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Price:$800";
+            document.getElementById("cell2_price").innerHTML=" &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Price:$500";
+            document.getElementById("cell3_price").innerHTML=" &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Price:$600";
+            document.getElementById("cell1_category").innerHTML="&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Watch";
+            document.getElementById("cell2_category").innerHTML="&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Watch";
+            document.getElementById("cell3_category").innerHTML="&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Watch";
+           document.getElementById("cell1_name").innerHTML="&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Redux";
+            document.getElementById("cell2_name").innerHTML="&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Cartney";
+            document.getElementById("cell3_name").innerHTML="&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Megalith";
+    
+
+            
+          }
+       function  ear_phone(){
+             document.getElementById("cell").innerHTML="Ear Phones";
+             document.getElementById("cell1").src="https://www.greatbuyz.com/blog/wp-content/uploads/2018/08/skullcandy.png";     
+            document.getElementById("cell2").src="https://www.autoncell.com/image/upload/v1559123940/xiaomi_sjii6x.jpg";
+             document.getElementById("cell3").src="https://i.pinimg.com/originals/d2/7f/29/d27f2966c4a72706d8101140ef755bb4.jpg";
+            document.getElementById("cell1_price").innerHTML=" &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Price:$200";
+            document.getElementById("cell2_price").innerHTML=" &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Price:$500";
+            document.getElementById("cell3_price").innerHTML=" &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Price:$100";
+            document.getElementById("cell1_category").innerHTML="&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Ear Phone";
+            document.getElementById("cell2_category").innerHTML="&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Ear Phone ";
+            document.getElementById("cell3_category").innerHTML="&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Ear Phone";
+         document.getElementById("cell1_name").innerHTML="&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Edifier";
+            document.getElementById("cell2_name").innerHTML="&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Cowin";
+            document.getElementById("cell3_name").innerHTML="&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Bose";
+            
+          }
+
+function image_detail_cell1(){ 
+  var image_src_cell1=document.getElementById("cell1").src;
+  var name_cell1=document.getElementById("cell1_name").textContent;
+  var category_cell1=document.getElementById("cell1_category").textContent;
+  var price_cell1=document.getElementById("cell1_price").textContent;
+  console.log(name_cell1);
+  
+  console.log(image_src_cell1);
+   document.getElementById("detail").src=image_src_cell1;
+  document.getElementById("detail_name").innerHTML=name_cell1;
+  document.getElementById("detail_price").innerHTML=price_cell1;
+  document.getElementById("detail_category").innerHTML=category_cell1;
+  document.getElementById("detail_text").innerHTML="Designed for comfort, Edifier headphones fit naturally on your head. Engineered to produce fantastic sound. All headphones have foldable features for easy transportation. Choose from a selection of colors and styles to suit your creativity.Edifier is a Chinese audio manufacturer that produces speakers, music systems, and headphones for personal & home entertainment.Designed for comfort, Edifier headphones fit naturally on your head. Engineered to produce fantastic sound. All headphones have foldable features for easy transportation. Choose from a selection of colors and styles to suit your creativity.Edifier is a Chinese audio manufacturer that produces speakers, music systems, and headphones for personal & home entertainment.Designed for comfort, Edifier headphones fit naturally on your head. Engineered to produce fantastic sound. All headphones have foldable features for easy transportation. Choose from a selection of colors and styles to suit your creativity.Edifier is a Chinese audio manufacturer that produces speakers, music systems, and headphones for personal & home entertainment.Designed for comfort, Edifier headphones fit naturally on your head. Engineered to produce fantastic sound. All headphones have foldable features for easy transportation. Choose from a selection of colors and styles to suit your creativity.Edifier is a Chinese audio manufacturer that produces speakers, music systems, and headphones for personal & home entertainment.";
+  
+ 
+}
+
+ 
+function image_detail_cell2(){ 
+  var image_src_cell2=document.getElementById("cell2").src;
+  var name_cell2=document.getElementById("cell2_name").textContent;
+  var category_cell2=document.getElementById("cell2_category").textContent;
+  var price_cell2=document.getElementById("cell2_price").textContent;
+  console.log(image_src_cell2);
+  document.getElementById("detail").src=image_src_cell2;
+  document.getElementById("detail_name").innerHTML=name_cell2;
+  document.getElementById("detail_price").innerHTML=price_cell2;
+  document.getElementById("detail_category").innerHTML=category_cell2;
+  document.getElementById("detail_text").innerHTML="Designed for comfort, Edifier headphones fit naturally on your head. Engineered to produce fantastic sound. All headphones have foldable features for easy transportation. Choose from a selection of colors and styles to suit your creativity.Edifier is a Chinese audio manufacturer that produces speakers, music systems, and headphones for personal & home entertainment.Designed for comfort, Edifier headphones fit naturally on your head. Engineered to produce fantastic sound. All headphones have foldable features for easy transportation. Choose from a selection of colors and styles to suit your creativity.Edifier is a Chinese audio manufacturer that produces speakers, music systems, and headphones for personal & home entertainment.Designed for comfort, Edifier headphones fit naturally on your head. Engineered to produce fantastic sound. All headphones have foldable features for easy transportation. Choose from a selection of colors and styles to suit your creativity.Edifier is a Chinese audio manufacturer that produces speakers, music systems, and headphones for personal & home entertainment.Designed for comfort, Edifier headphones fit naturally on your head. Engineered to produce fantastic sound. All headphones have foldable features for easy transportation. Choose from a selection of colors and styles to suit your creativity.Edifier is a Chinese audio manufacturer that produces speakers, music systems, and headphones for personal & home entertainment.";
+}
+
+function image_detail_cell3(){ 
+  var image_src_cell3=document.getElementById("cell3").src;
+  var name_cell3=document.getElementById("cell3_name").textContent;
+  var category_cell3=document.getElementById("cell3_category").textContent;
+  var price_cell3=document.getElementById("cell3_price").textContent;
+  console.log(image_src_cell3);
+  document.getElementById("detail").src=image_src_cell3;
+   document.getElementById("detail_name").innerHTML=name_cell3;
+  document.getElementById("detail_price").innerHTML=price_cell3;
+  document.getElementById("detail_category").innerHTML=category_cell3;
+  document.getElementById("detail_text").innerHTML="Designed for comfort, Edifier headphones fit naturally on your head. Engineered to produce fantastic sound. All headphones have foldable features for easy transportation. Choose from a selection of colors and styles to suit your creativity.Edifier is a Chinese audio manufacturer that produces speakers, music systems, and headphones for personal & home entertainment.Designed for comfort, Edifier headphones fit naturally on your head. Engineered to produce fantastic sound. All headphones have foldable features for easy transportation. Choose from a selection of colors and styles to suit your creativity.Edifier is a Chinese audio manufacturer that produces speakers, music systems, and headphones for personal & home entertainment.Designed for comfort, Edifier headphones fit naturally on your head. Engineered to produce fantastic sound. All headphones have foldable features for easy transportation. Choose from a selection of colors and styles to suit your creativity.Edifier is a Chinese audio manufacturer that produces speakers, music systems, and headphones for personal & home entertainment.Designed for comfort, Edifier headphones fit naturally on your head. Engineered to produce fantastic sound. All headphones have foldable features for easy transportation. Choose from a selection of colors and styles to suit your creativity.Edifier is a Chinese audio manufacturer that produces speakers, music systems, and headphones for personal & home entertainment.";
+  
+}
+
+
+
